052f87ba03265e3bbe8dc04d8f119c8e6ec0477f
Alicia Tran <aliciatran4550@gmail.com>
2024-09-24 21:27:45 -0400

    fix: directory path for saving images

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ for (const book of bookdata) {
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, './public/images/')
+		cb(null, './server/public/images/')
 	},
 	filename: function (req, file, cb) {
 		cb(null, setImageFileName(req))
